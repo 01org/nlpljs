81e52f8d4c6887e68d1bcbbfca750b09ee66052f
Max Waterman <max.waterman@intel.com>
2014-10-03 16:51:59 +0100

    Refactored rehostPage.js and fixed 'selfs' typo in content-push.html.

--- app/scripts/rehostPage.js
+++ app/scripts/rehostPage.js
@@ -43,56 +43,48 @@
     // create and open panel
     var currentTabUrl=document.URL;
     var extensionUrl=chrome.extension.getURL("");
-    var indexUrl=chrome.extension.getURL("index.html");
+    var cpUrl=chrome.extension.getURL("content-push/content-push.html");
+    var cssUrl=chrome.extension.getURL("styles/main.css");
 
     // remove document content and add new head and body
     document.removeChild(document.documentElement);
     var html = document.createElement('html');
     var head = document.createElement('head');
     var body = document.createElement('body');
+
+    var base = document.createElement('base');
+    base.setAttribute('href',extensionUrl);
+    head.appendChild(base);
+
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = cssUrl;
+    head.appendChild(link);
+
+    link = document.createElement('link');
+    link.rel = 'import';
+    link.href = cpUrl;
+    head.appendChild(link);
+
+    var cp = document.createElement('content-push');
+    cp.setAttribute("iframeurl", currentTabUrl);
+    cp.addEventListener('lineadd', function (e) {
+      port.postMessage(eventPageMessage('lineadd', e.detail));
+    });
+
+    cp.addEventListener('resetextractor', function (e) {
+      port.postMessage(eventPageMessage('resetextractor', e.detail));
+    });
+
+    cp.addEventListener('getkeywords', function (e) {
+      port.postMessage(eventPageMessage('getkeywords', e.detail));
+    });
+
+    body.appendChild(cp);
+
     html.appendChild(head);
     html.appendChild(body);
     document.appendChild(html);
-
-    /*Add new link/import element to head
-      to load the index.html content */
-    var link = document.createElement('link');
-    link.rel = 'import';
-    link.href = indexUrl;
-    link.onload = function(e) {
-      // copy head and body from import into main document
-      var link = document.querySelector('link[rel="import"]');
-      var content = link.import;
-
-      // Grab DOM from warning.html's document.
-      var head = content.querySelector('head');
-      var body = content.querySelector('body');
-
-      document.head.innerHTML=
-        '<base href="'+extensionUrl+'">\n'+
-        head.innerHTML;
-      document.body.innerHTML=body.innerHTML;
-
-      // set iframe url in <content-push> element
-      var cp = document.querySelector('content-push');
-      cp.setAttribute("iframeurl", currentTabUrl);
-
-      cp.addEventListener('lineadd', function (e) {
-        port.postMessage(eventPageMessage('lineadd', e.detail));
-      });
-
-      cp.addEventListener('resetextractor', function (e) {
-        port.postMessage(eventPageMessage('resetextractor', e.detail));
-      });
-
-      cp.addEventListener('getkeywords', function (e) {
-        port.postMessage(eventPageMessage('getkeywords', e.detail));
-      });
-      
-    };
-
-    link.onerror = function(e) { console.log("got link error"); };
-    document.head.appendChild(link);
 
     //send results back to event page
     //console.log("CS: sending done response");
