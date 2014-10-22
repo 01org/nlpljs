6e53e5bac7400a088be2ac29503e68c66e3b8881
Max Waterman <max.waterman@intel.com>
2014-10-22 17:19:23 +0100

    [cp-auth] Implemented cp-auth functionality.

--- app/scripts/event_page.js
+++ app/scripts/event_page.js
@@ -7,6 +7,7 @@ var localport;
 var workerMessage = function (type, data) {
   return JSON.stringify({ type: type, data: data }, null, 4);
 };
+var epAuth = {}; // authenticator for multiple services
 
 // Listens for the app launching then creates the window
 chrome.runtime.onInstalled.addListener(function() {
@@ -40,7 +41,7 @@ chrome.pageAction.onClicked.addListener(function(tab) {
       //Execute the content script
       chrome.tabs.executeScript(null, {file: "scripts/rehostPage.js"}, function(result) {
         if (chrome.runtime.lastError) {
-          console.log('EP:error executing content script:',chrome.runtime.lastError.message);
+          console.log('EP-MAIN:error executing content script:',chrome.runtime.lastError.message);
         }
       });
     } else {
@@ -87,3 +88,41 @@ var initChannel = function() {
     });
   });
 };
+
+/**
+ * message routing
+ *  component: ('auth'||...)
+ */
+chrome.runtime.onMessageExternal.addListener(function(message,sender,sendResponse) {
+  console.log('EP-MAIN:got message:',message);
+  var keepChannelOpen=false;
+  var component = message.component;
+  switch (component) {
+    case 'auth':
+      var service = message.service;
+
+      if (epAuth.hasOwnProperty(service)) {
+        var type = message.type;
+        switch (type) {
+          case 'gettoken':
+            keepChannelOpen = epAuth[service].getToken(sendResponse);
+            break;
+          case 'removecachedtoken':
+            var token = message.token;
+            epAuth[service].removeCachedToken(token);
+            break;
+          default:
+            console.log(new Error('EP-MAIN:unknown message type:'+type));
+            break;
+        }
+      } else {
+        console.log(new Error('EP-MAIN:unknown service:'+service));
+      }
+      break;
+    default:
+      console.log(new Error('EP-MAIN:unknown component:'+component));
+      break;
+  }
+
+  return keepChannelOpen; // to call sendResponse asynchronously
+});
