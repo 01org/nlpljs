ec62594159b3ba88abc36a57d62d080f973b6135
Elliot Smith <elliot.smith@intel.com>
2014-11-10 16:19:10 +0000

    [cp-formatter] Extract formatDate from cp-tile-file

--- app/content_push/cp-formatter.js
+++ app/content_push/cp-formatter.js
@@ -15,6 +15,24 @@
       }
     },
 
+    /* format a date string in format '2014-09-08T10:05:15.497Z'
+       and return just the YYYY-MM-DD */
+    formatDate: function (dateStr) {
+      try {
+        var dt = new Date(dateStr);
+        var month = dt.getMonth();
+        var day = dt.getDay();
+
+        month = (month < 10 ? '0' + month : month);
+        day = (day < 10 ? '0' + day : day);
+
+        return dt.getFullYear() + '-' + month + '-' + day;
+      } catch (e) {
+        return 'unknown';
+      }
+    },
+
+    /* extract the domain from a URL string */
     domainFromURL: function (url) {
       if (url) {
         var matches = DOMAIN_REGEX.exec(url);
