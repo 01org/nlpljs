c9893b40277c83391ad8169caa9aeefeae0202a7
Elliot Smith <elliot.smith@intel.com>
2014-11-12 16:17:45 +0000

    Use web worker for HTTP requests

---
+++ app/worker/http_worker.js
@@ -0,0 +1,31 @@
+onmessage = function (event) {
+  var request = event.data;
+  var requestId = request.id;
+  var url = request.url;
+
+  var xhr = new XMLHttpRequest();
+
+  xhr.open('GET', url, true);
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 /* DONE */) {
+      if (!(("" + xhr.status).match(/2\d\d/))) {
+        postMessage({
+          id: requestId,
+          response: {
+            error: this
+          }
+        });
+      }
+      else {
+        postMessage({
+          id: requestId,
+          requestUrl: url,
+          response: JSON.parse(this.responseText)
+        });
+      }
+    }
+  };
+
+  xhr.send();
+};
