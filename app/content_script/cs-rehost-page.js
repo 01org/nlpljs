d053c5899fb0bf199afe640da3eca7fd4ea2259e
Plamena Manolova <plamena.manolova@intel.com>
2014-11-24 14:43:16 +0000

    content-push: Remove nlp messaging from content script.

--- app/content_script/cs-rehost-page.js
+++ app/content_script/cs-rehost-page.js
@@ -2,14 +2,6 @@
 /* read the design doc for explaination on the comments */
 
   var open = true;
-  var port = null;
-
-  function eventPageMessage(type, data) {
-    return {
-      type: type,
-      data: data
-    };
-  }
 
   chrome.runtime.onMessage.addListener( function(message,sender,sendResponse) {
     if (message === "cp-toggle") {
@@ -18,24 +10,7 @@
     }
   });
 
-  chrome.runtime.sendMessage("cp-nlpinit");
-
-  port = chrome.runtime.connect();
-  port.onMessage.addListener(function(message) {
-      switch(message.message) {
-        case "keywordlist":
-          var event = new CustomEvent("keywordlist", {
-                      detail: message.data,
-                      bubbles: true,
-                      cancelable: true
-                    });
-
-          document.dispatchEvent(event);
-          break;
-        default:
-          console.log("Unknown message from event page");
-        }
-  });
+  chrome.runtime.sendMessage("cp-init");
 
   rehostPage();
 
@@ -85,21 +60,6 @@
 
     var cpMain = document.createElement('cp-main');
     cpMain.setAttribute('fit', '');
-    cpMain.addEventListener('lineadded', function (e) {
-      port.postMessage(eventPageMessage('lineadded', e.detail));
-    });
-
-    cpMain.addEventListener('newcontext', function (e) {
-      port.postMessage(eventPageMessage('newcontext', e.detail));
-    });
-
-    cpMain.addEventListener('getkeywords', function (e) {
-      port.postMessage(eventPageMessage('getkeywords', e.detail));
-    });
-
-    cpMain.addEventListener('processcontext', function (e) {
-      port.postMessage(eventPageMessage('processcontext', e.detail));
-    });
 
     body.appendChild(cpMain);
 
