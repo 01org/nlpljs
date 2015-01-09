3f448aac224cc1af6156c3314364bacb24b7703b
Elliot Smith <elliot.smith@intel.com>
2015-01-09 10:28:30 +0000

    [libnlp] Make loadable via nodejs

--- app/libnlp/libnlp.js
+++ app/libnlp/libnlp.js
@@ -1,16 +1,23 @@
-define(['tokenizer', 'postagger', 'keyphrase_extractor'], function (tokenizer,
-  postagger, keyphrase_extractor) {
-  var libnlp;
-
-  if (typeof libnlp !== 'undefined') {
-    return libnlp;
-  }
-
-  libnlp =  {
-    tokenizer: tokenizer,
-    postagger: postagger,
-    keyphrase_extractor: keyphrase_extractor
+(function () {
+  var makeLibnlp = function (tokenizer, postagger, keyphrase_extractor) {
+    return {
+      tokenizer: tokenizer,
+      postagger: postagger,
+      keyphrase_extractor: keyphrase_extractor
+    };
   };
 
-  return libnlp;
-});
+  if (typeof define !== 'undefined' && define.amd) {
+    define(
+      ['tokenizer', 'postagger', 'keyphrase_extractor'],
+      function (tokenizer, postagger, keyphrase_extractor) {
+        return makeLibnlp(tokenizer, postagger, keyphrase_extractor);
+      }
+    );
+  } else {
+    var tokenizer = require('./tokenizer');
+    var postagger = require('./postagger');
+    var keyphrase_extractor = require('./keyphrase_extractor');
+    module.exports = makeLibnlp(tokenizer, postagger, keyphrase_extractor);
+  }
+})();
