144c7e579bb984c848c144f4c6d01094076376a0
Elliot Smith <elliot.smith@intel.com>
2015-02-11 11:13:04 +0000

    Add coverage reporting for unit tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,10 +6,20 @@ module.exports = function (grunt) {
       all: {
         files: [
           { src: 'test/unit/*.test.js' }
-        ]
+        ],
+        reporter: 'dot',
+      },
+
+      cov: {
+        files: [
+          { src: 'test/unit/*.test.js' }
+        ],
+        reporter: 'html-cov',
+        reportDir: 'build'
       }
     }
   });
 
   grunt.registerTask('test', ['mochaccino:all']);
+  grunt.registerTask('cov', ['mochaccino:cov']);
 };
