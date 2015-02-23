45e163625fe03fbec8e2be8b09423376fb09bfdd
Plamena Manolova <plamena.manolova@intel.com>
2015-02-23 11:39:32 +0000

    extract-keyphrases: Include knuth-shuffle.

--- scripts/extract-keyphrases.sh
+++ scripts/extract-keyphrases.sh
@@ -11,26 +11,6 @@ var input = '';
 
 var hideScores = !!process.argv[2];
 
-// from http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-function shuffle (array) {
-  var currentIndex = array.length, temporaryValue, randomIndex ;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-};
-
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', function() {
@@ -59,7 +39,8 @@ process.stdin.on('end', function() {
 
   // randomly sort results if scores are hidden
   if (hideScores) {
-    shuffle(results);
+    var knuth_shuffle = require('knuth-shuffle');
+    knuth_shuffle.knuthShuffle(results);
   } else {
     results.sort();
   }
