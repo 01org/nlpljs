d465d5d970bd76b01899e798152dc9f19fc6e0f1
Plamena Manolova <plamena.manolova@intel.com>
2015-04-29 14:11:54 +0100

    Fix shell script license comment.

--- scripts/extract-keyphrases.sh
+++ scripts/extract-keyphrases.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 # Natural Language Processing Library for JavaScript
 #
 # A client-side NLP utility library for web applications
@@ -23,7 +25,6 @@
 #   Max Waterman <max.waterman@intel.com>
 #   Plamena Manolova <plamena.manolova@intel.com>
 
-#!/usr/bin/env node
 // extract keywords from a piece of text on the command line;
 // echo or cat something into this script to see the results, e.g.
 // echo "the blue sun hates all interfering mind parasites" | ./extract.sh
