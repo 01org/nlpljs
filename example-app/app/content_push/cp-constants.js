fa1e1cb371eb9b0c5a1139f077d6be5d75407ed5
Max Waterman <max.waterman@intel.com>
2015-09-02 11:24:55 +0100

    Removing curly brackets from on- markup

--- example-app/app/content_push/cp-constants.js
+++ example-app/app/content_push/cp-constants.js
@@ -24,18 +24,23 @@
  *   Max Waterman <max.waterman@intel.com>
  *   Plamena Manolova <plamena.manolova@intel.com>
  */
+
 (function () {
   'use strict';
 
   window.CP_CONSTANTS = {
     /* icons for services which can serve files */
-    SERVICE_ICONS: { 'Drive': 'cloud' },
+    SERVICE_ICONS: {
+      'Drive': 'cloud'
+    },
+
     /* mapping from mime type to user-friendly document type */
     MIME_TYPES: {
       'application/vnd.google-apps.document': 'Google Docs',
       'text/plain': 'Text file'
     },
+
     /* timeout for images to load within (ms) */
     TIMEOUT: 5000
   };
-}());
+})();
